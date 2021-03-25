de2a33dc816ad676d0463bb4a1692f939a9452f9
Artem <47538574+Penultimo@users.noreply.github.com>
2021-03-25 20:31:24 +0200

    44

--- src/main/kotlin/13ExClass.kt
+++ src/main/kotlin/13ExClass.kt
@@ -1,4 +1,10 @@
 fun main(args: Array<String>) {
-    for(i in 10 downTo 2 step 3)
-        println("$i You are the best")
+    var puzzlePieces = 10
+    var puzzlePlaced = 0
+    while(puzzlePlaced < puzzlePieces){
+        puzzlePieces++
+        println(puzzlePieces)
+    }
+
+
 }
