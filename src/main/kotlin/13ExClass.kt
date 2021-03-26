3aa654bb08a10016c3ab1250dfa0b6a8e937f729
Artem <47538574+Penultimo@users.noreply.github.com>
2021-03-26 19:44:37 +0200

    55

--- src/main/kotlin/13ExClass.kt
+++ src/main/kotlin/13ExClass.kt
@@ -1,8 +1,5 @@
 fun main(args: Array<String>) {
   var cats = listOf("Tiger", "Smoky", "Sassy", "Patch", "Sammy")
-    var index = 10
-    while(index < cats.size) {
-        println("Hello, ${cats[index]}")
-        index++
-    }
+    var index = 0
+    
 }
