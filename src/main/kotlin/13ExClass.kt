6c9b9f51ab39c2405e12fcb84d8208bd685a4d34
Artem <47538574+Penultimo@users.noreply.github.com>
2021-04-01 13:02:09 +0300

    3241221

--- src/main/kotlin/13ExClass.kt
+++ src/main/kotlin/13ExClass.kt
@@ -1,7 +1,16 @@
 fun main(args: Array<String>) {
+    var finished = false
     do{
         println("Please input the number")
         var input = readLine()?:"0"
         var number = input.toInt()
-    } while (number <100)
+        if(number > 100){
+            println("Thanks!")
+            finished = true
+        } else {
+            println("$number is not greater than 100")
+
+        }
+    } while (!finished)
+
 }
