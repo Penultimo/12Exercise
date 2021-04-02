4abd885ce5f45cb6803504eaa36f6f563068d3fb
Artem <47538574+Penultimo@users.noreply.github.com>
2021-04-02 18:39:33 +0300

    23123

--- src/main/kotlin/13ExClass.kt
+++ src/main/kotlin/13ExClass.kt
@@ -1,7 +1,22 @@
 fun main(args: Array<String>) {
-        println("Please input the number")
-        var input = readLine()?:"0"
+        println("Please input the number which factorial is less than 3 000 000")
+        do{
+        var finished = false
+        var input = readLine() ?: "0"
         var number = input.toInt()
+        var result = 1L
+        var index = 1
+        while(index < number) {
+                result *= index
+                index++
+        }
+        println(result)
+        if(result < 3000000) {
+                finished = true
+                println("Factorial of $input is $result. It is LESS than 3 000 000. Thanks")
+        }
+            else {println("Factorial of $input is $result. It is MORE than 3 000 000. Please enter another number")}
 
+    }while(!finished)
 
 }
