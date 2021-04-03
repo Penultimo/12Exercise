6efa35e7b5a9c82f2e0f517668c4e358ecafb7c6
Artem <47538574+Penultimo@users.noreply.github.com>
2021-04-03 11:15:10 +0300

    234234

--- src/main/kotlin/13ExClass.kt
+++ src/main/kotlin/13ExClass.kt
@@ -1,21 +1,19 @@
 fun main(args: Array<String>) {
-        println("Please input the number which factorial is less than 3 000 000")
-        do{
-        var finished = false
-        var input = readLine() ?: "0"
-        var number = input.toInt()
-        var result = 1L
-        var index = 1
-        while(index < number) {
-                result *= index
-                index++
+    val usernames = hashSetOf("john", "bob", "alice")
+    do{
+        println("Please choose and write below your username:")
+        var input = readLine()?:""
+        var userName = input.toString()
+        var finished =  false
+        if(usernames.contains(userName)){
+            println("This name $userName already is taken. Please choose another username.")
+
+        } else{
+            usernames.add(userName)
+            println("Thanks for registration, $userName. Your name is added to the list of clients.")
+            finished = true
+            println(usernames)
         }
-        println(result)
-        if(result < 3000000) {
-                finished = true
-                println("Factorial of $input is $result. It is LESS than 3 000 000. Thanks")
-        }
-            else {println("Factorial of $input is $result. It is MORE than 3 000 000. Please enter another number")}
 
     }while(!finished)
 
