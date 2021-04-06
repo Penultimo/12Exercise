6ee8047e408b56c279ab367a97492b5447875edd
Artem <47538574+Penultimo@users.noreply.github.com>
2021-04-06 20:51:00 +0300

    11

--- src/main/kotlin/13ExClass.kt
+++ src/main/kotlin/13ExClass.kt
@@ -1,28 +1,9 @@
 fun main(args: Array<String>) {
-//    println("Please enter the name of an animal")
-//    var input = readLine()?:""
-//    var name = input.toString()
-    var name = "cat"
-    println(lifeCycle("cat"))
-}
-
-fun lifeCycle(name: String):Int {
-        when (name) {
-        "cat" -> {
-            var age = 15
-        }
-//        "dogs" -> {
-//            var age = 10
-//        }
-//        "rabbit" -> {
-//            var age = 12
-//        }
-//        "everything else" -> {
-//            var age = 20
-//        }
-    }
 
 }
+
+fun getSizeMessage(message: String)
+
 
 
 
