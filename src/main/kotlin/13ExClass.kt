7d199f783dca8eb08aeff583a4a4caef3a116976
Artem <47538574+Penultimo@users.noreply.github.com>
2021-04-08 09:56:28 +0300

    2312

--- src/main/kotlin/13ExClass.kt
+++ src/main/kotlin/13ExClass.kt
@@ -1,9 +1,15 @@
-fun main(args: Array<String>) {
-
+fun main (args: Array<String>){
+    printHello()
 }
 
-fun getSizeMessage(message: String)
-
-
-
-
+fun printHello(){
+    fun sayHi (name: String){
+        println("Hi, $name, how are you?")
+    }
+    while(true){
+        println("please enter a name")
+        val name = readLine()?:""
+        if (name == "") break
+        sayHi(name)
+    }
+}
